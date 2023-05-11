043a6ef2aeafd13afbc0dccf14ac8ee9ddef6307
datngdev <datngpd@gmail.com>
2023-05-11 22:39:35 +0700

    Init Base Project

---
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.chatapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chatapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+    }
+}
