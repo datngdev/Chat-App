e1bd3cfe78afe09ab374bccc55ccd91c5433f932
datngdev <datngpd@gmail.com>
2023-05-17 13:05:20 +0700

    fix LoginSharedPreference use interface

--- app/src/main/java/com/example/chatapp/datas/sharedpreferences/LoginSharedPreference.kt
+++ app/src/main/java/com/example/chatapp/datas/sharedpreferences/LoginSharedPreference.kt
@@ -1,23 +1,9 @@
 package com.example.chatapp.datas.sharedpreferences
 
 import android.content.Context
-import android.content.Context.MODE_PRIVATE
 
-class LoginSharedPreference {
-
-    fun getCurrentUserId(context: Context): String? {
-        val pref = context.getSharedPreferences("PREF", MODE_PRIVATE)
-        return pref.getString("currentUser", "")
-    }
-
-    fun updateLoginStatus(context: Context, userId: String) {
-        val pref = context.getSharedPreferences("PREF", MODE_PRIVATE)
-        pref.edit().putString("currentUser", userId).commit()
-    }
-
-    fun logout(context: Context) {
-        val pref = context.getSharedPreferences("PREF", MODE_PRIVATE)
-        pref.edit().putString("currentUser", "").commit()
-    }
-
+interface LoginSharedPreference {
+    fun getCurrentUserId(context: Context): String?
+    fun updateLoginStatus(context: Context, userId: String)
+    fun logout(context: Context)
 }
