a8e19c790db664ab3a7bd26d11d4e9ccb7bd1277
datngdev <datngpd@gmail.com>
2023-05-17 13:14:04 +0700

    change LoginSharedPreference DI

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -5,13 +5,14 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import com.example.chatapp.databinding.ActivityMainBinding
 import com.example.chatapp.datas.sharedpreferences.LoginSharedPreference
+import com.example.chatapp.datas.sharedpreferences.LoginSharedPreferenceImpl
 import com.example.chatapp.ui.home.HomeFragment
 import com.example.chatapp.ui.login.LoginFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    private val loginSharedPreference = LoginSharedPreference()
+    private val loginSharedPreference: LoginSharedPreference = LoginSharedPreferenceImpl()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
