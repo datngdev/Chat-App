0a901279c50204ab31e1748a569fb6d235b1e33f
datng_dev <88136857+datngdev@users.noreply.github.com>
2023-05-18 14:44:04 +0700

    Revert "add new login feature"

--- app/src/main/java/com/example/chatapp/datas/models/User.kt
+++
@@ -1,7 +0,0 @@
-package com.example.chatapp.datas.models
-
-data class User(
-    var name: String,
-    var avatar: String,
-    var isActive: Boolean
-)
--- app/src/main/java/com/example/chatapp/datas/sharedpreferences/LoginSharedPreference.kt
+++
@@ -1,9 +0,0 @@
-package com.example.chatapp.datas.sharedpreferences
-
-import android.content.Context
-
-interface LoginSharedPreference {
-    fun getCurrentUserId(): String?
-    fun updateLoginStatus(userId: String)
-    fun logout()
-}
